711d6d870216ce32e663433ef0e6e7592289e1e4
Mikkel Christensen <mikkelchristensen@CONGENICA.come>
2021-12-07 15:24:55 +0000

    change to indefinit loop

--- study.py
+++ study.py
@@ -1,5 +1,7 @@
 import time
 time_stamp = time.time()
 log_file = './log/study.log'
-with open(log_file, 'w') as file_handle:
+file_handle = open(log_file, 'w')
+while True:
     file_handle.write(str(time_stamp) + '\n')
+    time.sleep(60)
